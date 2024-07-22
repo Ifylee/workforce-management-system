ae283cc06581c7f125e853a9e3767ca41d66f8ce
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-07-22 08:13:08 -0500

    added more sql queries

--- db/index.js
+++ db/index.js
@@ -20,6 +20,57 @@ function quit(){
     
 }
 
+function viewEmployees() {
+    sqlQuery = `SELECT employee.id, employee.first_name,
+            employee.last_name, role.title, department.name AS department,
+            role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+            FROM employee JOIN role on employee.role_id = role.id
+            JOIN department on role.department_id = department.id
+            JOIN employee manager on employee.manager_id = manager.id;`;
+            pool.query(sqlQuery, (err, results) => {
+                console.log("\n");
+                console.table(results.rows)
+                loadMainMenu();
+            }) 
+}
+
+function viewEmployeesByDepartment() {
+    let sqlQuery = `SELECT * FROM department`; 
+    pool.query( sqlQuery, (err, results) => {
+       if(err) {
+            console.log(err);
+       }
+
+       const departmentChoices = results.rows.map(department => ({
+            name: department.name,
+            value: department.id
+
+       })) 
+
+       
+    inquirer.prompt([{
+        list: 'list',
+        name: 'departmentId',
+        message: 'Which department would you like to view their employees?',
+        choices: departmentChoices
+    }]).then(({departmentId}) => {
+        sqlQuery = `SELECT employee.id, employee.first_name,
+        employee.last_name, role.title
+        FROM employee JOIN role on employee.role_id = role.id
+        JOIN department on role.department_id = department.id
+        WHERE department.name = $1;`
+        pool.query(sqlQuery, [departmentId], (err, results) => {
+            console.log("\n");
+                 console.table(results.rows)
+                 loadMainMenu();  
+            });
+
+        });
+
+
+    });
+
+}
 
 function loadMainMenu() {
     inquirer.prompt([{
@@ -46,23 +97,19 @@ function loadMainMenu() {
         let { choice } = answers;
 
         if(choice === "VIEW_EMPLOYEES") {
-            sqlQuery = `SELECT employee.id, employee.first_name,
-            employee.last_name, role.title, department.name AS department,
-            role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
-            FROM employee JOIN role on employee.role_id = role.id
-            JOIN department on role.department_id = department.id
-            JOIN employee manager on employee.manager_id = manager.id;`;
-            pool.query(sqlQuery, (err, results) => {
-                console.log("\n");
-                console.table(results.rows)
-                loadMainMenu();
-            }) 
-        } else {
+            viewEmployees()
+        }
+        else if(choice === "VIEW_EMPLOYEES_BY_DEPARTMENT") {
+            viewEmployeesByDepartment()
+        }
+        else {
             quit();
         }   
 
     });  
 }
+
+
 
 function init() {
     console.log("Welcome to Workforce Management System");
--- db/query.sql
+++ db/query.sql
@@ -4,3 +4,10 @@ role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
 FROM employee JOIN role on employee.role_id = role.id
 JOIN department on role.department_id = department.id
 JOIN employee manager on employee.manager_id = manager.id;
+
+
+SELECT employee.id, employee.first_name,
+employee.last_name, role.title
+FROM employee JOIN role on employee.role_id = role.id
+JOIN department on role.department_id = department.id
+WHERE department.name = 'Engineering';
